b82a41f543c73fbbab81a0849302c4940f96380d
Darmawansyah murfa <darmawansyahmurfa@gmail.com>
2025-09-18 21:52:58 +0800

    Add files via upload

---
+++ praktikum 1/tempCodeRunnerFile.java
@@ -0,0 +1,14 @@
+public class ContohIfElse {
+    public static void main(String[] args) {
+        String nilai = "pulpena";
+
+        if (nilai == ("hp")) {
+            System.out.println("saya ingin hp");
+        } else if (nilai == ("pulpen")) {
+            System.out.println("saya ingin pulpen");
+        } else {
+            System.out.println("saya ingin id card");
+        }
+    }
+}
+
